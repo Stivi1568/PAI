2cf70223e4c6ac41a14fe84d90373cee0c807827
Stivi1568 <75080495+Stivi1568@users.noreply.github.com>
2022-09-14 15:41:32 +0200

    Add files via upload

--- cos.php
+++ cos.php
@@ -4,9 +4,17 @@
 <body>
 
 	<?php
-		$mycounter = 1;
-		echo "Hello World" . "<br>" . $mycounter . "<br>" . "Hello Friend"
+		$x = 0;
+
+		if ($x == 0) {
+			echo "Nie masz wiadomosci";
+		}
+		else{
+			echo "masz" . $x  . "Wiadomości";
+		}
 	?>
+
+
 
 
 </body>
